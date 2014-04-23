fdd50d2643ed75d4f7cf8454b2e38cd746024a71
Pedro Garau Martinez <iinfernape1998@gmail.com>
2014-04-23 16:41:07 +0200

    Fixed AudioB1
    Fixed AudioB1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-    sh device/huawei/msm7x27a-common/patches/apply.sh
-    sh device/huawei/u8950/patches/apply.sh
+sh device/huawei/msm7x27a-common/patches/apply.sh
+sh device/huawei/u8950/patches/apply.sh
